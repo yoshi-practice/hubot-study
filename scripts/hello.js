5c27498f2973f4f631520b00662ddf26872b5e61
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-02-22 18:34:22 +0900

    Add omikuji func

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,11 @@
 'use strict';
+
 module.exports = (robot) => {
-  robot.hear(/hello>/i, (msg) => {
+  robot.hear(/おみくじ>/i, (msg) => {
+    const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+
     const user_id = msg.message.user.id;
-    msg.send(`Hello, <@${user_id}>`);
+    msg.send(`${lot}, <@${user_id}>`);
   });
 };
